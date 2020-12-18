d8b821c0b6b786fe3918c357b58eede2c88f549f
Adam Giebl <adamgiebl@gmail.com>
2020-12-18 03:23:31 +0100

    background, logo

--- src/script.js
+++ src/script.js
@@ -1,3 +1,15 @@
 import './styles/style.scss'
 
 console.log('Works')
+
+const movementStrength = 25
+const height = movementStrength / window.innerHeight
+const width = movementStrength / window.innerWidth
+document.querySelector('body').addEventListener('mousemove', e => {
+  const pageX = e.pageX - window.innerWidth / 2
+  const pageY = e.pageY - window.innerHeight / 2
+  const newvalueX = width * pageX * -1 - 25
+  const newvalueY = height * pageY * -1 - 50
+  console.log(newvalueX, newvalueY)
+  document.querySelector('.stars').style.backgroundPosition = newvalueX + 'px ' + newvalueY + 'px'
+})
