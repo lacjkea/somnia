db794a87292048c1862cbab41958f618c54f4935
Adam Giebl <adamgiebl@gmail.com>
2020-12-19 22:03:47 +0100

    audio section

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import './styles/style.scss'
+import Plyr from 'plyr'
 
 console.log('Works')
 
@@ -10,6 +11,28 @@ document.querySelector('body').addEventListener('mousemove', e => {
   const pageY = e.pageY - window.innerHeight / 2
   const newvalueX = width * pageX * -1 - 25
   const newvalueY = height * pageY * -1 - 50
-  console.log(newvalueX, newvalueY)
   document.querySelector('.stars').style.backgroundPosition = newvalueX + 'px ' + newvalueY + 'px'
+})
+
+document.querySelectorAll('audio').forEach(el => {
+  console.log(el.classList)
+  new Plyr('.' + el.classList[0], {})
+})
+
+// calculate the correct offset of an element relative to the window
+const getOffset = element => {
+  const bound = element.getBoundingClientRect()
+  const doc = document.documentElement
+
+  return {
+    top: bound.top + window.pageYOffset - doc.clientTop,
+    left: bound.left + window.pageXOffset - doc.clientLeft
+  }
+}
+
+const features = document.querySelectorAll('.feature')
+document.querySelectorAll('.circle-group').forEach((g, i) => {
+  const moveToLeft = features[i].offsetWidth / 2
+  features[i].style.left = `${getOffset(g).left - (moveToLeft - 50)}px`
+  features[i].style.top = `${getOffset(g).top + 80}px`
 })
