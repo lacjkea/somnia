4a5902f4ace7a165a93294555b8e602480da4f0a
Adam Giebl <adamgiebl@gmail.com>
2020-12-20 19:25:31 +0100

    Updates

--- script.baf0e655.js
+++ script.baf0e655.js
@@ -4745,7 +4745,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64170" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49622" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
