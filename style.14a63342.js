04b6ad440924a16cfe51a0f53718b15903cfdbd4
Adam Giebl <adamgiebl@gmail.com>
2020-12-20 21:46:32 +0100

    Updates

--- style.14a63342.js
+++ style.14a63342.js
@@ -184,7 +184,12 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"src/styles/style.scss":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"C:\\Users\\adamg\\projects\\somnia\\static\\Kefa.ttc":[["Kefa.1bef6185.ttc","static/Kefa.ttc"],"static/Kefa.ttc"],"C:\\Users\\adamg\\projects\\somnia\\static\\wave.svg":[["wave.4863e949.svg","static/wave.svg"],"static/wave.svg"],"C:\\Users\\adamg\\projects\\somnia\\static\\stars.svg":[["stars.069146f0.svg","static/stars.svg"],"static/stars.svg"],"C:\\Users\\adamg\\projects\\somnia\\static\\purple-blob.svg":[["purple-blob.f4e90e31.svg","static/purple-blob.svg"],"static/purple-blob.svg"],"_css_loader":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -389,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/index.js.map
+//# sourceMappingURL=/style.14a63342.js.map
